3ee3257a397d71977c1c42e60c40283d1384679e
Anant Gaire <56.anant@gmail.com>
2021-02-23 07:42:32 +0545

    firebase

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyAdYccNUop3JtlWvMQug2CExlt0rl6m9Vw',
   authDomain: 'signal-clone-5a50a.firebaseapp.com',
@@ -6,3 +10,14 @@ const firebaseConfig = {
   messagingSenderId: '685518797831',
   appId: '1:685518797831:web:6ead7a545df59c99a1bea2',
 };
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
-import { Button, Input } from 'react-native-elements';
-
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
 import { KeyboardAvoidingView } from 'react-native';
 
 const RegisterScreen = ({ navigation }) => {
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackTitle: 'Back to Login',
+    });
+  }, [navigation]);
+
+  const register = () => {};
+
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <StatusBar style="light" />
@@ -23,7 +34,35 @@ const RegisterScreen = ({ navigation }) => {
           value={name}
           onChangeText={(text) => setName(text)}
         />
+        <Input
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Input
+          placeholder="Profile Picture URL (optional) "
+          type="text"
+          value={imageUrl}
+          onChangeText={(text) => setImageUrl(text)}
+          onSubmitEditing={register}
+        />
       </View>
+
+      <Button
+        containerStyle={styles.button}
+        raised
+        onPress={register}
+        title="Register"
+      />
+      <View style={{ height: 100 }} />
     </KeyboardAvoidingView>
   );
 };
@@ -31,5 +70,18 @@ const RegisterScreen = ({ navigation }) => {
 export default RegisterScreen;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+  inputContainer: {
+    width: 300,
+  },
 });
