74ab4203934ac19fe1126197195cd987c6fb0074
Anant Gaire <56.anant@gmail.com>
2021-02-25 11:56:35 +0545

    chat screen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -133,7 +133,7 @@ const ChatScreen = ({ navigation, route }) => {
                     <Text style={styles.recieverText}>{data.message}</Text>
                   </View>
                 ) : (
-                  <View style={styles.sender}>
+                  <View key={id} style={styles.sender}>
                     <Avatar
                       position="absolute"
                       // WEB
